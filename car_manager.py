aa4736a90aed0d254294134540eff6729f9122a8
Mohamed Dawod <107262252+modawod93@users.noreply.github.com>
2024-10-27 16:44:36 +0300

    Add files via upload

---
+++ car_manager.py
@@ -0,0 +1,37 @@
+from turtle import Turtle
+import random
+
+COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
+STARTING_MOVE_DISTANCE = 6
+MOVE_INCREMENT = 7
+
+
+class CarManager:
+    def __init__(self):
+        self.all_cars = []
+        self.car_speed = STARTING_MOVE_DISTANCE
+        self.car_spawn_chance = 7  # Starting probability divisor
+
+    def create_car(self):
+        # Adjust random chance based on level
+        random_chance = random.randint(1, self.car_spawn_chance)
+        
+        if random_chance == 1:
+            new_car = Turtle("square")
+            new_car.shapesize(stretch_wid=1, stretch_len=2)
+            new_car.penup()
+            new_car.color(random.choice(COLORS))
+            random_y = random.randint(-250, 250)
+            new_car.goto(300, random_y)
+            self.all_cars.append(new_car)
+
+    def move_cars(self):
+        for car in self.all_cars:
+            car.backward(self.car_speed)
+    
+    def level_up(self):
+        self.car_speed += MOVE_INCREMENT
+        # Decrease the divisor to increase car spawn rate, making it more likely to spawn a car each time
+        if self.car_spawn_chance > 1:
+            self.car_spawn_chance -= 2
+
